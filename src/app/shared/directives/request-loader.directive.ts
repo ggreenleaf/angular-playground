f089cfcdd9bc5c72bdac2f2ff1160f17dbc2df02
ggreenleaf <geoffrey.greenleaf@gmail.com>
2018-10-14 10:36:16 -0500

    added more comments

--- src/app/shared/directives/request-loader.directive.ts
+++ src/app/shared/directives/request-loader.directive.ts
@@ -30,6 +30,9 @@ export class RequestLoaderDirective {
     private componentFactoryResolver: ComponentFactoryResolver
   ) { }
 
+
+  // we probably could combine the following two functions into one and pass the component Type and its inputs if needed
+  // this would allow more control and maybe later allow custom error handling component by an input prop
   private createLoadingComponent() {
     // if you don't clear it will append to end 
     // maybe thats how ngFor works?
